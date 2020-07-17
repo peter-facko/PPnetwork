88d654888e4a161d5b075406fb0d796cb80e1418
Peter Fačko <pfacko1@gmail.com>
2020-07-17 16:38:34 +0200

    now uses ThreadLocal for parameter object array

--- PPnetwork/Descriptor.cs
+++ PPnetwork/Descriptor.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Reflection;
+using System.Threading;
 
 namespace PPnetwork
 {
@@ -11,7 +14,10 @@ namespace PPnetwork
 	/// <typeparam name="OrderHandler">Specific Order Handler, e.g. MyConnection</typeparam>
 	class Descriptor<OrderHandlerBase, OrderBase, OrderHandler> : IInvoker<OrderHandlerBase, OrderBase>
 		where OrderHandler : OrderHandlerBase
+		where OrderBase : notnull
 	{
+		readonly ThreadLocal<object[]> objectArray = new ThreadLocal<object[]>(() => new object[1]);
+
 		readonly MethodInfo orderHandlerMethod;
 		/// <summary>
 		/// Creates the Descriptor from the generic type of the OHI
@@ -32,8 +38,9 @@ namespace PPnetwork
 			// Invoke throws exceptions packed in a TargetInvocationException
 			try
 			{
-				// TODO: cache these one object arrays
-				orderHandlerMethod.Invoke(orderHandler, new object[] { order! });
+				var arr = objectArray.Value!;
+				arr[0] = order;
+				orderHandlerMethod.Invoke(orderHandler, arr);
 			}
 			catch (TargetInvocationException e)
 			{
